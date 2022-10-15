a7fee9deb01e165454a5c9ea603a2f88a29912e3
diegososa <sosadiego28@gmail.com>
2022-10-15 14:23:46 -0300

    entrega5

--- js/cart.js
+++ js/cart.js
@@ -21,12 +21,26 @@ document.addEventListener('DOMContentLoaded', async function(){
             <img src="${productosCarrito[i].image}" alt="" style="height:50px; width:auto;">
             </div>
             <p class="col-2">${productosCarrito[i].name}</p>
-            <p class="col-2">${productosCarrito[i].currency} ${productosCarrito[i].unitCost}</p>
-            <div class="p0 col-2"><input type="text" class="form-control"  style="width: 30%;" value="${productosCarrito[i].count}"></div>
-            <p class="col-2"><b>${productosCarrito[i].currency} ${productosCarrito[i].unitCost}</b></p>
+            <p class="col-2">${productosCarrito[i].currency} <span id="costo_id">${productosCarrito[i].unitCost}</span></p>
+            <div class="p0 col-2"><input type="number" class="form-control"  style="width: 30%;" value="${productosCarrito[i].count}" id="id_input"></div>
+            <p class="col-2"><b>${productosCarrito[i].currency}  <span id="id_subtotal">${productosCarrito[i].unitCost} </span></b></p>
         </div>
         `;
     };
     productos.innerHTML = listaProductos;
+
+    let input = document.getElementById("id_input");
+    
+    input.addEventListener('change', updateValue);
+    
+    function updateValue(){
+        let costo = document.getElementById("costo_id");
+        let subtotal;
+        subtotal = input.value * costo.innerText;
+        let subtotal_html = document.getElementById("id_subtotal");
+        subtotal_html.innerText= subtotal;
+    }
 });
+
+
 
